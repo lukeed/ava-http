d0c8adacd4478a5cc64f8f12316248327e6d57e3
luke <luke.edwards05@gmail.com>
2016-02-04 10:24:02 -0800

    server interaction (get) tests. await vs thennale

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 'use strict';
 import test from '../';
+import {listen, send, json} from './server';
+console.log(json);
 
 test('add http context methods to Test object', async t => {
 	const http = t.context.http;
@@ -12,9 +14,19 @@ test('add http context methods to Test object', async t => {
 	}
 });
 
-test('use http.get to fetch json', async t => {
-	// t.plan(1);
-	await t.context.http.get('http://www.google.com').then(data => {
-		console.log(data);
+test('http.get, await: 200 <String>', async t => {
+	// spin up the test server
+	const url = await listen(async (req, res) => send(res, 200, 'await'));
+	// get the response by waiting
+	const data = await t.context.http.get(url);
+	t.same(data, 'await');
+});
+
+test('http.get, thennable: 200 <String>', async t => {
+	// spin up the test server
+	const url = await listen(async (req, res) => send(res, 200, 'thennable'));
+	// get the response with .then()
+	t.context.http.get(url).then(data => {
+		t.same(data, 'thennable');
 	});
 });
