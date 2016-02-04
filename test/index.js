daffded24b4c452eced058e5d6f2d7f950e93923
luke <luke.edwards05@gmail.com>
2016-02-04 10:31:34 -0800

    pull test payloads to constants. add first json test & ensure works on non-async server

--- test/index.js
+++ test/index.js
@@ -3,6 +3,9 @@ import test from '../';
 import {listen, send, json} from './server';
 console.log(json);
 
+const str = 'ava-http';
+const obj = {a: 'b'};
+
 test('add http context methods to Test object', async t => {
 	const http = t.context.http;
 	const assertion = k => {
@@ -16,17 +19,29 @@ test('add http context methods to Test object', async t => {
 
 test('http.get, await: 200 <String>', async t => {
 	// spin up the test server
-	const url = await listen(async (req, res) => send(res, 200, 'await'));
+	const url = await listen(async (req, res) => send(res, 200, str));
 	// get the response by waiting
-	const data = await t.context.http.get(url);
-	t.same(data, 'await');
+	const res = await t.context.http.get(url);
+	t.same(res, str);
 });
 
 test('http.get, thennable: 200 <String>', async t => {
 	// spin up the test server
-	const url = await listen(async (req, res) => send(res, 200, 'thennable'));
+	const url = await listen(async (req, res) => send(res, 200, str));
 	// get the response with .then()
-	t.context.http.get(url).then(data => {
-		t.same(data, 'thennable');
+	t.context.http.get(url).then(res => {
+		t.same(res, str);
 	});
+});
+
+test('http.get: 200 <Object>)', async t => {
+	const url = await listen(async (req, res) => send(res, 200, obj));
+	const res = await t.context.http.get(url, {json: true});
+	t.same(res, obj);
+});
+
+test('http.get, server is not async', async t => {
+	const url = await listen((req, res) => send(res, 200, obj));
+	const res = await t.context.http.get(url, {json: true});
+	t.same(res.a, 'b');
 });
