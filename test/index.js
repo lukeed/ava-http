16445dc5939f6378c78146c623482ad40a8d636a
luke <luke.edwards05@gmail.com>
2016-02-05 01:33:05 -0800

    update tests for new http export. switch to string responses (equal vs deepEqual)

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 'use strict';
-import test from '../dist';
+import test from 'ava';
+import http from '../src';
 import {listen, send, json} from './server';
 
 const str = 'ava-http';
@@ -10,11 +11,12 @@ const obj = {a: 'b'};
  * Initialize on `test.before()`
  */
 
-test('add http context methods to Test object', async t => {
-	const http = t.context.http;
+test('import http object', async t => {
 	const assertion = k => {
 		return t.true(http.hasOwnProperty(k) && (typeof http[k] === 'function'), `http.${k} added`);
 	};
+
+	t.true(typeof http === 'object', 'http object exists');
 
 	for (let key of ['get', 'post', 'put', 'del']) {
 		assertion(key);
@@ -25,25 +27,21 @@ test('http.get: async/await', async t => {
 	// spin up the test server
 	const url = await listen(async (req, res) => send(res, 200, str));
 	// get the response by waiting
-	const res = await t.context.http.get(url);
-	t.same(res, str);
+	t.is(await http.get(url), str);
 });
 
 test('http.get: generator/yield', function * (t) {
 	// spin up the test server
 	const url = yield listen((req, res) => send(res, 200, str));
 	// get the response by waiting
-	const res = yield t.context.http.get(url);
-	t.same(res, str);
+	t.is(yield http.get(url), str);
 });
 
 test('http.get: thennable', async t => {
 	// spin up the test server
 	const url = await listen(async (req, res) => send(res, 200, str));
 	// get the response with .then()
-	t.context.http.get(url).then(res => {
-		t.same(res, str);
-	});
+	http.get(url).then(res => t.is(res, str));
 });
 
 /**
@@ -52,39 +50,31 @@ test('http.get: thennable', async t => {
 
 test('http.get: String <200>', async t => {
 	const url = await listen(async (req, res) => send(res, 200, str));
-	const res = await t.context.http.get(url);
-	t.ok(typeof res === 'string');
-	t.same(res, str);
+	t.is(await http.get(url), str);
 });
 
 test('http.get: Object String <200>', async t => {
 	const url = await listen(async (req, res) => send(res, 200, obj));
-	const res = await t.context.http.get(url, {json: false});
+	const res = await http.get(url, {json: false});
 	t.true(typeof res === 'string');
-	t.same(res, JSON.stringify(obj));
+	t.is(res, JSON.stringify(obj));
 });
 
 test('http.get: Object <200>', async t => {
 	const url = await listen(async (req, res) => send(res, 200, obj));
-	const res = await t.context.http.get(url);
-	t.true(typeof res === 'object');
-	t.same(res, obj);
+	t.same(await http.get(url), obj);
 });
 
 test('http.get: with Parameters <200>', async t => {
 	const url = await listen(async (req, res) => send(res, 200, obj));
 
 	const params = {token: '123456789'};
-	const res = await t.context.http.get(url, {params});
-
-	t.true(typeof res === 'object');
-	t.same(res, obj);
+	t.same(await http.get(url, {params}), obj);
 });
 
 test('http.get: server is not async', async t => {
 	const url = await listen((req, res) => send(res, 200, obj));
-	const res = await t.context.http.get(url);
-	t.same(res, obj);
+	t.same(await http.get(url), obj);
 });
 
 /**
@@ -94,41 +84,32 @@ test('http.get: server is not async', async t => {
 test('http.post: Success <200>', async t => {
 	const url = await listen(async (req, res) => send(res, 200, obj));
 	// use postResponse to get full Reponse object on success
-	const res = await t.context.http.postResponse(url);
-	t.same(res.statusCode, 200);
+	const res = await http.postResponse(url);
+	t.is(res.statusCode, 200);
 });
 
 test('http.post: Bad Request <400>', async t => {
-	const url = await listen(async (req, res) => send(res, 400, obj));
-	t.context.http.post(url).catch(err => {
-		t.same(err.statusCode, 400);
-	});
+	const url = await listen(async (req, res) => send(res, 400));
+	http.post(url).catch(err => t.is(err.statusCode, 400));
 });
 
 test('http.post: JSON Object <200>', async t => {
-	const url = await listen(async (req, res) => send(res, 200, obj));
+	const url = await listen(async (req, res) => send(res, 200, str));
 	const body = {some: 'payload'};
-	const res = await t.context.http.post(url, {body});
-	t.same(res, obj);
+	t.is(await http.post(url, {body}), str);
 });
 
 test('http.post: Form Object <200>', async t => {
-	const url = await listen(async (req, res) => send(res, 200, obj));
+	const url = await listen(async (req, res) => send(res, 200, str));
 	const form = {some: 'payload'}; // will be urlencoded
-	const res = await t.context.http.post(url, {form});
-	t.same(res, obj);
+	t.is(await http.post(url, {form}), str);
 });
 
 test('http.post: Bad JSON <400>', async t => {
-	const url = await listen(async (req, res) => {
-		const data = await json(req);
-		send(res, 200, data.nothing);
-	});
+	const url = await listen(async req => await json(req));
 
 	const body = '{ "bad json" }';
-	t.context.http.post(url, {body}).catch(err => {
-		t.same(err.statusCode, 400);
-	});
+	http.post(url, {body}).catch(err => t.is(err.statusCode, 400));
 });
 
 /**
@@ -136,43 +117,33 @@ test('http.post: Bad JSON <400>', async t => {
  */
 
 test('http.put: Success <200>', async t => {
-	const url = await listen(async (req, res) => send(res, 200, obj));
+	const url = await listen(async (req, res) => send(res, 200));
 	// use putResponse to get full Reponse object on success
-	const res = await t.context.http.putResponse(url);
-	t.same(res.statusCode, 200);
+	const res = await http.putResponse(url);
+	t.is(res.statusCode, 200);
 });
 
 test('http.put: Bad Request <400>', async t => {
-	const url = await listen(async (req, res) => send(res, 400, obj));
-	t.context.http.put(url).catch(err => {
-		t.same(err.statusCode, 400);
-	});
+	const url = await listen(async (req, res) => send(res, 400));
+	http.put(url).catch(err => t.is(err.statusCode, 400));
 });
 
 test('http.put: JSON Object <200>', async t => {
-	const url = await listen(async (req, res) => send(res, 200, obj));
+	const url = await listen(async (req, res) => send(res, 200, str));
 	const body = {some: 'payload'};
-	const res = await t.context.http.put(url, {body});
-	t.same(res, obj);
+	t.is(await http.put(url, {body}), str);
 });
 
 test('http.put: Form Object <200>', async t => {
-	const url = await listen(async (req, res) => send(res, 200, obj));
+	const url = await listen(async (req, res) => send(res, 200, str));
 	const form = {some: 'payload'}; // will be urlencoded
-	const res = await t.context.http.put(url, {form});
-	t.same(res, obj);
+	t.is(await http.put(url, {form}), str);
 });
 
 test('http.put: Bad JSON <400>', async t => {
-	const url = await listen(async (req, res) => {
-		const data = await json(req);
-		send(res, 200, data.nothing);
-	});
-
+	const url = await listen(async req => await json(req));
 	const body = '{ "bad json" }';
-	t.context.http.put(url, {body}).catch(err => {
-		t.same(err.statusCode, 400);
-	});
+	http.put(url, {body}).catch(err => t.is(err.statusCode, 400));
 });
 
 /**
@@ -180,31 +151,25 @@ test('http.put: Bad JSON <400>', async t => {
  */
 
 test('http.del: Success <200>', async t => {
-	const url = await listen(async (req, res) => send(res, 200, obj));
+	const url = await listen(async (req, res) => send(res, 200));
 	// use delResponse to get full Reponse object on success
-	const res = await t.context.http.delResponse(url);
-	t.same(res.statusCode, 200);
+	const res = await http.delResponse(url);
+	t.is(res.statusCode, 200);
 });
 
 test('http.del: Bad Request <400>', async t => {
-	const url = await listen(async (req, res) => send(res, 400, obj));
-	t.context.http.del(url).catch(err => {
-		t.same(err.statusCode, 400);
-	});
+	const url = await listen(async (req, res) => send(res, 400));
+	http.del(url).catch(err => t.is(err.statusCode, 400));
 });
 
 test('http.del: Unauthorized <401>', async t => {
-	const url = await listen(async (req, res) => send(res, 401, obj));
-	t.context.http.del(url).catch(err => {
-		t.same(err.statusCode, 401);
-	});
+	const url = await listen(async (req, res) => send(res, 401));
+	http.del(url).catch(err => t.is(err.statusCode, 401));
 });
 
 test('http.del: Not Found <404>', async t => {
-	const url = await listen(async (req, res) => send(res, 404, obj));
-	t.context.http.del(url).catch(err => {
-		t.same(err.statusCode, 404);
-	});
+	const url = await listen(async (req, res) => send(res, 404));
+	http.del(url).catch(err => t.is(err.statusCode, 404));
 });
 
 /**
@@ -214,29 +179,25 @@ test('http.del: Not Found <404>', async t => {
 test('Response: 200 <Code>', async t => {
 	const url = await listen(async (req, res) => send(res, 200));
 	// Use getResponse for full data. Added to Error responses by default.
-	const res = await t.context.http.getResponse(url);
-	t.same(res.statusCode, 200);
+	const res = await http.getResponse(url);
+	t.is(res.statusCode, 200);
 });
 
 test('Response: 301 <Code>', async t => {
 	const url = await listen(async (req, res) => send(res, 301));
-	t.context.http.get(url).catch(err => {
-		t.same(err.statusCode, 301);
+	http.get(url).catch(err => {
+		t.is(err.statusCode, 301);
 	});
 });
 
 test('Response: 404 <Code>', async t => {
 	const url = await listen(async (req, res) => send(res, 404));
-	t.context.http.get(url).catch(err => {
-		t.same(err.statusCode, 404);
-	});
+	http.get(url).catch(err => t.is(err.statusCode, 404));
 });
 
 test('Response: 404 <Object>', async t => {
 	const url = await listen(async (req, res) => send(res, 404, obj));
-	t.context.http.get(url).catch(err => {
-		t.same(err.response.body, obj);
-	});
+	http.get(url).catch(err => t.same(err.response.body, obj));
 });
 
 test('Response: 500 <Custom>', async t => {
@@ -249,8 +210,8 @@ test('Response: 500 <Custom>', async t => {
 		onError: async (req, res) => send(res, 200, str)
 	});
 
-	t.context.http.get(url).catch(err => {
-		t.same(err.response.body, str);
-		t.same(err.statusCode, 200);
+	http.get(url).catch(err => {
+		t.is(err.response.body, str);
+		t.is(err.statusCode, 200);
 	});
 });
